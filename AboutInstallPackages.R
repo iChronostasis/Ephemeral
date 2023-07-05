45c57d2f7d085f8c2f70577d4028c6843199a10a
Chronostasis <82665896+iChronostasis@users.noreply.github.com>
2023-07-05 09:10:43 +0800

    Update AboutInstallPackages.R

--- AboutInstallPackages.R
+++ AboutInstallPackages.R
@@ -1,1 +1,12 @@
+# R Package for batch installation
+packages <- c("deSolve","bbmle","stats","ggplot2","broom","tidyverse", "mgcv")
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+ipak(packages)
+# R Package for batch calls
+lapply(packages, function(x){library(x, character.only = T)}) 
 
